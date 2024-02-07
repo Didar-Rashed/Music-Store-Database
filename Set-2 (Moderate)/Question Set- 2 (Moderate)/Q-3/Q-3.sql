519b9b365bb7cf01e0531507778931d8483d5d7b
Didarul Islam <158755071+Didar-Rashed@users.noreply.github.com>
2024-02-07 12:32:26 +0600

    Delete Set-2 (Moderate)/Question Set- 2 (Moderate) directory

--- Set-2 (Moderate)/Question Set- 2 (Moderate)/Q-3/Q-3.sql
+++
@@ -1,9 +0,0 @@
---Q-3: Return all the track names that have a song length longer than the avetage song length. Return the name and milliseconds for each track. Order by the song length with sthe longest songs listed first
-
-SELECT name, milliseconds
-FROM track
-WHERE milliseconds > (
-	SELECT AVG(milliseconds) AS avg_track_length
-	From track
-)
-ORDER BY milliseconds DESC
